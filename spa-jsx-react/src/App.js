8400709f98d808a04ebc780d887a6e91dc471503
PaulinaLL <paulinalll90@wp.pl>
2020-03-23 15:41:11 +0100

    working map

--- spa-jsx-react/src/App.js
+++ spa-jsx-react/src/App.js
@@ -6,33 +6,41 @@ function App() {
   return (
     <div className="App">
       <main className="App-main">
-        <div className="card">
-          <div className="foto-name">
-            <img src={data[0].avatar} className="" alt="profil-pic" />
-            <div className="name-title">
-              <h1>
-                {data[0].firstName} {data[0].lastName}
-              </h1>
-              <p>{data[0].title}</p>
-            </div>
-          </div>
-          <div className="contact-data">
-            <img src="https://img.icons8.com/carbon-copy/100/000000/phone.png" />
-            <p>{data[0].contact.phone}</p>
-            <img src="https://img.icons8.com/dotty/80/000000/email.png" />
-            <p>{data[0].contact.email}</p>
-            <img src="https://img.icons8.com/wired/64/000000/domain.png" />
-            <a
-              className="App-link"
-              href={data[0].contact.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {data[0].contact.url}
-            </a>
-          </div>
-          <p>member since: {data[0].memberSince}</p>
-        </div>
+        <ul>
+          {data.map(person => {
+            return (
+              <li key={person.id}>
+                <div className="card">
+                  <div className="foto-name">
+                    <img src={person.avatar} className="" alt="profil-pic" />
+                    <div className="name-title">
+                      <h1>
+                        {person.firstName} {person.lastName}
+                      </h1>
+                      <p>{person.title}</p>
+                    </div>
+                  </div>
+                  <div className="contact-data">
+                    <img src="https://img.icons8.com/carbon-copy/100/000000/phone.png" />
+                    <p>{person.contact.phone}</p>
+                    <img src="https://img.icons8.com/dotty/80/000000/email.png" />
+                    <p>{person.contact.email}</p>
+                    <img src="https://img.icons8.com/wired/64/000000/domain.png" />
+                    <a
+                      className="App-link"
+                      href={person.contact.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {person.contact.url}
+                    </a>
+                  </div>
+                  <p>member since: {person.memberSince}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </div>
   );
